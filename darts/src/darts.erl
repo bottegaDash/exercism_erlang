99e5dbfadc9bb2cb19cd753620362c5c9504e436
davidjash2 <davidjash2@gmail.com>
2020-04-28 20:18:12 -0400

    darts: Completed solution

--- darts/src/darts.erl
+++ darts/src/darts.erl
@@ -3,4 +3,14 @@
 -export([score/2]).
 
 
-score(_X, _Y) -> undefined.
+score(_X, _Y) -> 
+    if
+        _X*_X + _Y*_Y =< 1*1 ->
+            10;
+        _X*_X + _Y*_Y =< 5*5 ->
+            5;
+        _X*_X + _Y*_Y =< 10*10 ->
+            1;
+        true ->
+            0
+    end.
